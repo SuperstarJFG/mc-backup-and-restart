d6f974a3c925970c572bbc855c236ae75659aaed
Jake Gresh <85658632+SuperstarJFG@users.noreply.github.com>
2025-05-11 01:28:48 -0700

    Add +x permission in setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Add +x permission to the script
+chmod +x backup_and_restart.sh
 # Add a cron job to run the backup_and_restart.sh script every day at 10:00 AM
 (crontab -l 2>/dev/null; echo "0 10 * * * ~/mc-backup-and-restart/backup_and_restart.sh >> ~/mc-backup-and-restart/backup_and_restart.log 2>&1") | crontab -
